bcca38c2bde8be563c5906dbd926585646ccfe90
Fagundes1993 <mfbl93@gmail.com>
2024-06-16 10:26:18 -0300

    Exercícios do módulo de Python

---
+++ 04 - Python/02-variaveis.py
@@ -0,0 +1,22 @@
+# Variáveis
+
+# 1. Variáveis
+
+idade = 30 # criando um variavel
+
+print(idade)
+
+nome = ('Matheus Fagundes')
+
+print(nome)
+
+idade = 30                  #int
+altura = 1.80               #float
+nome = 'Matheus Fagundes'   #String
+estudando = True            #Boolean
+
+# Obtendo dados do usuário e sealvando em variáveis
+
+linguagem = input('Qual é a linguagem de programação que você está estudando?')
+
+print('A linguagem que você está estudando é: ', linguagem)
---
+++ 04 - Python/03-operadores.py
@@ -0,0 +1,25 @@
+# Operações Aritméticas
+
+numero1 = 10
+numero2 = 20
+
+print(numero1 + numero2) #Soma
+print(numero1 - numero2) #Subtração
+print(numero1 * numero2) #Multiplicação
+print(numero1 / numero2) #Divisão
+print(numero1 // numero2) #Divisão inteira
+print(20 % 3) # Módulo
+print(2 ** 3) # Potênciação
+
+# Operadores Booleanos
+
+a = 5
+b = 10
+c = 5
+
+print(a > b)    #Maior que
+print(a < b)    #Menor que
+print(a >= c)   #Maior ou Igual
+print(a <= c)   #Menor ou Igual
+print('Python' == 'python') #Igualdade
+print('abacaxi' != 'banana') #Diferença
