172e6365f78ce852bf3b49029b804747d008139b
Fagundes1993 <mfbl93@gmail.com>
2024-06-16 12:56:10 -0300

    Aua de conversão de tipos

---
+++ 04 - Python/04-conversao-de-tipos.py
@@ -0,0 +1,16 @@
+# Conversão de Tipos
+
+idade = '26'
+
+print(idade, type(idade))
+
+idade_inteira = int(idade)
+
+print(idade_inteira, type(idade_inteira))
+
+# Funções de conversão
+
+# int()
+# str()
+# float()
+# bool()
